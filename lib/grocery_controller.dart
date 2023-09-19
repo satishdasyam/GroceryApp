12543b0300f054227412ce86263c7fa279f373b4
Satish Dasyam <satishdasyam@gmail.com>
2023-09-19 18:05:20 +0530

    Move list click navigation to method

--- lib/grocery_controller.dart
+++ lib/grocery_controller.dart
@@ -10,5 +10,4 @@ class GroceryController {
     return fetchProductsUseCase.fetchGroceryList();
   }
 
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -19,20 +19,6 @@ class MyApp extends StatelessWidget {
       title: 'Groceries',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
@@ -89,12 +75,7 @@ class _GroceryHomePageState extends State<GroceryHomePage> {
                     padding: const EdgeInsets.all(10),
                     child: ListTile(
                       onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  ProductDetailPage(item: productList[index])),
-                        );
+                        navigateToDetailsPage(index);
                       },
                       title: Text(
                         productList[index].title,
@@ -129,6 +110,15 @@ class _GroceryHomePageState extends State<GroceryHomePage> {
               child: CircularProgressIndicator(),
             ))
       ]),
+    );
+  }
+
+  void navigateToDetailsPage(int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) =>
+              ProductDetailPage(item: productList[index])),
     );
   }
 }
